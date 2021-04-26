f6cfe26f76a2b03fb210f0ed855604c9f944be86
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2021-04-25 20:57:14 -0400

    test

--- cmake/SDL2ConfigBackup.cmake
+++ cmake/SDL2ConfigBackup.cmake
@@ -3,48 +3,24 @@ cmake_minimum_required(VERSION 3.13)
 execute_process(
     COMMAND sdl2-config --version
     OUTPUT_VARIABLE sdl2-version
-    ERROR_VARIABLE sdl2-version-error
 )
 
-project(SDL2 LANGUAGES C)# VERSION ${sdl2-version})
+foreach(flag version cflags libs static-libs)
+    execute_process(
+        COMMAND sdl2-config --${flag}
+        OUTPUT_VARIABLE sdl2-${flag}
+    )
+    string(REPLACE " " ";" sdl2-${flag} ${sdl2-${flag}})
+endforeach()
+
+project(SDL2 LANGUAGES C VERSION ${sdl2-version})
+
+set(CMAKE_REQUIRED_FLAGS 
+    "$<IF:$<CXX_COMPILER_ID:MSVC>,/std:,-std=>${CMAKE_CXX_STANDARD}"
+)
 
 add_library(SDL2 INTERFACE IMPORTED)
 add_library(SDL2-static INTERFACE IMPORTED)
-
-execute_process(
-    COMMAND sdl2-config --cflags
-    OUTPUT_VARIABLE sdl2-cflags
-)
-
-execute_process(
-    COMMAND sdl2-config --libs
-    OUTPUT_VARIABLE sdl2-libs
-)
-
-
-execute_process(
-    COMMAND sdl2-config --static-libs
-    OUTPUT_VARIABLE sdl2-static-libs
-)
-
-string(REPLACE " " ";" sdl2-cflags ${sdl2-cflags})
-string(REPLACE " " ";" sdl2-libs ${sdl2-libs})
-string(REPLACE " " ";" sdl2-static-libs ${sdl2-static-libs})
-
-message(STATUS ${sdl2-version})
-message(STATUS ${sdl2-cflags})
-message(STATUS ${sdl2-libs})
-message(STATUS ${sdl2-static-libs})
-
-# find_library(SDL2-library NAMES SDL2)
-# find_library(SDL2-main NAMES SDL2main)
-
-# target_link_libraries(SDL2 INTERFACE ${SDL2-library} ${SDL2-main})
-# target_link_libraries(SDL2-static INTERFACE ${SDL2-library} ${SDL2-main})
-
-# find_path(sdl2-incl NAMES "SDL2/SDL.h")
-# target_include_directories(SDL2 INTERFACE ${sdl2-incl})
-# target_include_directories(SDL2-static INTERFACE ${sdl2-incl})
 
 target_compile_options(SDL2 INTERFACE "${sdl2-cflags}")
 target_compile_options(SDL2-static INTERFACE "${sdl2-cflags}")
